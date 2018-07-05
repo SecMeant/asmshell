2b4fba549f87e065c324e75cf33ea3d12d0d8e8d
secmeant <patryk.wlazlyn@gmail.com>
2018-07-05 18:17:37 +0200

    Adding main py script for asmshell
    Updating readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # asmshell
+
+Returns hex values of compiled assembly.
+
+## Script requires nasm to work. https://www.nasm.us/
---
+++ asmshell.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python3
+import os
+from subprocess import run
+
+INPUT_FILE = 'input.asm'
+
+# cuts off last extension
+OUTPUT_FILE = ''.join(INPUT_FILE.split('.')[:-1])
+
+def disas(mnemo):
+    # Writes user input to file to comC:\Users\hlz\Desktop\asmshellpile it with nasm
+    f = open(INPUT_FILE, 'w')
+    f.write(mnemo)
+    f.close()
+    
+    # Compilation
+    # If successfull nasm will return binary in
+    # file called the same but without .asm
+    try:
+        ret = run(['nasm', INPUT_FILE])
+        os.remove(INPUT_FILE)
+    except FileNotFoundError:
+        print('Error: You need to have nasm installed to make asmshell work.')
+        return
+
+    if ret.returncode != 0:
+        return
+    
+    # Compilation was successfull
+    # Reading nasm output and printing to user
+    f = open(OUTPUT_FILE, 'rb')
+    out = []
+    for byte in f.read():
+        # Collect binary as hex values without 0x prefix
+        # in 2 nibble format
+        out.append('{:02x}'.format(byte))
+
+    f.close()
+    os.remove(OUTPUT_FILE)
+
+    if len(out) == 0:
+        return
+
+    print('Code: ', ' '.join(out))
+
+def main():
+    while True:
+        print('asmshell > ', end='')
+        userin = input().lower()
+        if userin == 'exit' or userin == 'quit':
+            return
+
+        disas(mnemo=userin)
+
+if __name__ == '__main__':
+    main()
